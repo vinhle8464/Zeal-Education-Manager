64c3109519309811c4086b57c8c8c9ca3b6bf875
tbao1810 <55305069+tbao1810@users.noreply.github.com>
2021-07-11 03:01:49 +0700

    fix

--- ProjectSemester3/Areas/Faculty/Controllers/ExaminationController.cs
+++ ProjectSemester3/Areas/Faculty/Controllers/ExaminationController.cs
@@ -35,28 +35,47 @@ namespace ProjectSemester3.Areas.Faculty.Controllers
         [Route("examanalysis")]
         public IActionResult Examanalysis(string facultyid,string examid)
         {
-            ViewBag.faculty = ViewBag.account = examinationService.getfacultyid(facultyid);
-            ViewBag.examdetail = examinationService.examdetail(examid);
-            ViewBag.highestscore = examinationService.examdetail(examid).Marks.Max(m => m.Mark1).ToString();
-            ViewBag.lowestscore = examinationService.examdetail(examid).Marks.Min(m => m.Mark1).ToString();
-            int i = 0;
-            int numbermark = 0;
-            foreach (var mark in examinationService.examdetail(examid).Marks)
+            try
             {
-                numbermark++;
-                if (((decimal)mark.Mark1 / (decimal)mark.MaxMark)*100 >= (decimal)mark.Rate)
+                ViewBag.faculty = ViewBag.account = examinationService.getfacultyid(facultyid);
+                ViewBag.examdetail = examinationService.examdetail(examid);
+                ViewBag.highestscore = examinationService.examdetail(examid).Marks.Max(m => m.Mark1).ToString();
+                ViewBag.lowestscore = examinationService.examdetail(examid).Marks.Min(m => m.Mark1).ToString();
+                int i = 0;
+                int numbermark = 0;
+                foreach (var mark in examinationService.examdetail(examid).Marks)
                 {
-                    i++;
-                }
+                    numbermark++;
+                    if (((decimal)mark.Mark1 / (decimal)mark.MaxMark) * 100 >= (decimal)mark.Rate)
+                    {
+                        i++;
+                    }
 
+                }
+                ViewBag.passpercent = ((decimal)((i * 100) / numbermark));
+                ViewBag.pass = i;
+                ViewBag.failpercent = (decimal)100 - ViewBag.passpercent;
+                ViewBag.fail = numbermark - i;
+                ViewBag.maxscore = Convert.ToInt32(examinationService.examdetail(examid).Marks.Max(m => m.MaxMark));
+                ViewBag.mediumscore = Convert.ToInt32(examinationService.examdetail(examid).Marks.Sum(m => m.Mark1) / (decimal)examinationService.examdetail(examid).Marks.Count());
+                return View("examanalysis");
             }
-            ViewBag.passpercent = ((decimal)((i * 100) / numbermark));
-            ViewBag.pass = i;
-            ViewBag.failpercent = (decimal)100 - ViewBag.passpercent;
-            ViewBag.fail = numbermark - i;
-            ViewBag.maxscore =Convert.ToInt32( examinationService.examdetail(examid).Marks.Max(m => m.MaxMark));
-            ViewBag.mediumscore = Convert.ToInt32(examinationService.examdetail(examid).Marks.Sum(m => m.Mark1)/(decimal)examinationService.examdetail(examid).Marks.Count());
-            return View("examanalysis");
+            catch
+            {
+                ViewBag.faculty = ViewBag.account = examinationService.getfacultyid(facultyid);
+                ViewBag.examdetail = examinationService.examdetail(examid);
+                ViewBag.highestscore =0;
+                ViewBag.lowestscore =0;
+                
+                ViewBag.passpercent =0;
+                ViewBag.pass = 0;
+                ViewBag.failpercent =0;
+                ViewBag.fail =0;
+                ViewBag.maxscore = 0;
+                ViewBag.mediumscore = 0;
+                return View("examanalysis");
+            }
+           
         }
     }
 }
